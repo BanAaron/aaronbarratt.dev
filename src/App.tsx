212b9a6c48e1fbaf9112740e8b00693f970ed87e
Aaron Barratt <aarontbarratt@gmail.com>
2025-07-22 03:45:16 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className={'container drop-outline'}>
+            <h1>Aaron Barratt, the Developer</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
